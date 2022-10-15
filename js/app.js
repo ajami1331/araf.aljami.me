34c36a656db47389c3ff988bea44585faa635c8c
Araf Al Jami <clown1331@gmail.com>
2022-10-15 18:14:31 +0700

    Update

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ const http = require('http');
 const reload = require('reload');
 const express = require("express");
 const distDir = __dirname + "/dist/";
+console.log(distDir)
 const app = express();
 app.use(express.static(distDir));
 const server = http.createServer(app)
--- src/pages/blog.md
+++ src/pages/blog.md
@@ -3,3 +3,4 @@ layout: markdown
 Title: Posts
 ---
 >> - [First Post, Hello World](/blogs/first-post-hello-world/)
+>> - [What is life?](/blogs/what-is-life?/)
